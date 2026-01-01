eae626f08a1cee80769f1057fb4182937de64c34
Rupanjali <152796385+rupanjali-bharti@users.noreply.github.com>
2026-01-01 14:24:36 +0530

    Refactor Navbar to use Link instead of anchor tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,40 +3,45 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm px-4">
-        <a className="navbar-brand fw-bold fs-3 text-primary" href="/">
+    <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm px-4">
+      <div className="container-fluid">
+        <Link className="navbar-brand fw-bold fs-3 text-primary" to="/">
           Food<span className="text-success">Lovers</span>
-        </a>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav align-items-center">
+          <ul className="navbar-nav align-items-center text-center">
             <li className="nav-item mx-2">
-              <a className="nav-link fw-semibold" href="/">Home</a>
+              <Link className="nav-link fw-semibold" to="/">Home</Link>
             </li>
             <li className="nav-item mx-2">
-              <a className="nav-link fw-semibold" href="/view">View Recipes</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link fw-semibold" href="/search">Search Recipe</a>
-            </li>
-
-            <li className="nav-item mx-2">
-              <a className="nav-link fw-semibold" href="/add">Add Recipe</a>
+              <Link className="nav-link fw-semibold" to="/view">View Recipes</Link>
             </li>
             <li className="nav-item mx-2">
-              <a className="nav-link fw-semibold" href="/delete">Delete Recipe</a>
+              <Link className="nav-link fw-semibold" to="/search">Search Recipe</Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link className="nav-link fw-semibold" to="/add">Add Recipe</Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link className="nav-link fw-semibold" to="/delete">Delete Recipe</Link>
             </li>
           </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
